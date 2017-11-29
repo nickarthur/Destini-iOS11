d87e6d7d4e82960d0f6f1884cb4b501a5f6fddef
Larry Nickerson <lnickers@austin.rr.com>
2017-11-28 20:57:44 -0600

    committing code changes

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
     let answer1a = "I\'ll hop in. Thanks for the help!"
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyTextView: UILabel!
     
     // TODO Step 5: Initialise instance variables here
-    
+    var storyIndex : Int = 1
     
     
     
@@ -45,21 +45,53 @@ class ViewController: UIViewController {
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: UIControlState.normal)
+        bottomButton.setTitle(answer1b, for: .normal)
     }
-
     
-    // User presses one of the buttons
+    
+    // User hide the story buttons
+    fileprivate func hideButtons() {
+        topButton.isHidden = true
+        bottomButton.isHidden = true
+    }
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
         
-    
+        // TODO Step 4: Write an IF-Statement to update the views
+        // advance to story panel 3
+        if ((storyIndex == 1 || storyIndex == 2) && sender.tag == 1) {
+            storyIndex = 3
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: UIControlState.normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+        }
+            // advance to story panel 2
+        else if (storyIndex == 1 && sender.tag == 2) {
+            storyIndex = 2
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: UIControlState.normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        }
+            // advance to story panel 6
+        else if (storyIndex == 3 && sender.tag == 1) {
+            storyIndex = 6
+            storyTextView.text = story6
+            hideButtons()
+        }
+            // advance to story  panel 5
+        else if (storyIndex == 3 && sender.tag == 2) {
+            storyIndex = 5
+            storyTextView.text = story5
+            hideButtons()
+        }
+            // advance to story panel 4
+        else if (storyIndex == 2 && sender.tag == 2) {
+            storyIndex = 4
+            storyTextView.text = story4
+            hideButtons()
+        }
     }
-    
-
-
-
 }
 
